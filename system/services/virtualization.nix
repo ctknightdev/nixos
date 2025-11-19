c3a18252554a588ddf193b180b334046df5253db
Chris <git@ccox.dev>
2025-11-18 19:43:00 -0500

    Switch to using `podman` instead of `docker`

--- system/services/virtualization.nix
+++ system/services/virtualization.nix
@@ -4,25 +4,30 @@
 }:
 
 {
-  virtualisation.containers.enable = true;
-  virtualisation.docker = {
-    enable = true;
-    rootless = {
+  virtualisation = {
+    containers.enable = true;
+    podman = {
       enable = true;
-      setSocketVariable = true;
+      dockerCompat = true; # Aliases `docker` command to `podman`
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
   };
 
-  users.users.chris.extraGroups = [ "docker" ];
+  users.users.chris.extraGroups = [
+    "podman"
+  ];
 
   environment.systemPackages = with pkgs; [
-    docker
-    docker-compose
-    lazydocker
+    # Podman utilities for container management
+    podman
+    podman-compose
+    podman-tui
 
+    # QEMU-based virtualization tools
     qemu
     quickemu
 
+    # Distrobox for running distributions in containers
     distrobox
   ];
 }
