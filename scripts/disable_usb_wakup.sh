9660cef32da435b4eb99321aad75e128dc847e79
Christopher Knight <git@ccox.dev>
2026-01-03 00:13:19 -0500

    Update script to work with either `sudo` or `doas`.

--- scripts/disable_usb_wakup.sh
+++ scripts/disable_usb_wakup.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-# Disable USB device 1-3 from waking up the system
-echo "disabled" | sudo tee /sys/bus/usb/devices/1-3/power/wakeup > /dev/null
+if ! command -v sudo >/dev/null 2>&1
+then
+    # Disable USB device 1-3 from waking up the system
+    echo "disabled" | doas tee /sys/bus/usb/devices/1-3/power/wakeup > /dev/null
 
-# Check if 1-3.1 exists and disable it if it does
-if [ -f "/sys/bus/usb/devices/1-3.1/power/wakeup" ]; then
-    echo "disabled" | sudo tee /sys/bus/usb/devices/1-3.1/power/wakeup > /dev/null
-fi
+    # Check if 1-3.1 exists and disable it if it does
+    if [ -f "/sys/bus/usb/devices/1-3.1/power/wakeup" ]; then
+        echo "disabled" | doas tee /sys/bus/usb/devices/1-3.1/power/wakeup > /dev/null
+    fi
 
-# Check if 1-3.4 exists and disable it if it does
-if [ -f "/sys/bus/usb/devices/1-3.4/power/wakeup" ]; then
-    echo "disabled" | sudo tee /sys/bus/usb/devices/1-3.4/power/wakeup > /dev/null
+    # Check if 1-3.4 exists and disable it if it does
+    if [ -f "/sys/bus/usb/devices/1-3.4/power/wakeup" ]; then
+        echo "disabled" | doas tee /sys/bus/usb/devices/1-3.4/power/wakeup > /dev/null
+    fi
+else
+    # Disable USB device 1-3 from waking up the system
+    echo "disabled" | sudo tee /sys/bus/usb/devices/1-3/power/wakeup > /dev/null
+
+    # Check if 1-3.1 exists and disable it if it does
+    if [ -f "/sys/bus/usb/devices/1-3.1/power/wakeup" ]; then
+        echo "disabled" | sudo tee /sys/bus/usb/devices/1-3.1/power/wakeup > /dev/null
+    fi
+
+    # Check if 1-3.4 exists and disable it if it does
+    if [ -f "/sys/bus/usb/devices/1-3.4/power/wakeup" ]; then
+        echo "disabled" | sudo tee /sys/bus/usb/devices/1-3.4/power/wakeup > /dev/null
+    fi
 fi
