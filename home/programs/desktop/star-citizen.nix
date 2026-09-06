544731e643f4b52d3fdd57c89207814f552a4f5b
Christopher Knight <git@ccox.dev>
2026-09-06 01:37:48 -0400

    Update monitor resolution.

--- home/programs/desktop/star-citizen.nix
+++ home/programs/desktop/star-citizen.nix
@@ -6,7 +6,7 @@
   xdg.desktopEntries = {
     star-citizen = {
       name = "Star Citizen";
-      exec = "gamescope -f -w 3840 -h 1600 -W 3840 -H 1600 --force-grab-cursor -- steam-run rsi-launcher %U";
+      exec = "gamescope -f -w 2560 -h 1440 -W 2560 -H 1440 --force-grab-cursor -- steam-run rsi-launcher %U";
       icon = "rsi-launcher";
       comment = "Roberts Space Industries Launcher for the game Star Citizen";
       terminal = false;
